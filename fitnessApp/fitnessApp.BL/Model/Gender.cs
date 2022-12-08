418ee38aef0f1c6da534d4d8097e7c354f84b407
LopatkinDA <LopatkinDA@quarta.su>
2022-12-08 16:52:41 +0300

    day2

---
+++ fitnessApp/fitnessApp.BL/Model/Gender.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Authentication.ExtendedProtection.Configuration;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fitnessApp.BL.Model
+{/// <summary>
+/// делать XML комментарии надо ко всем public методам
+/// </summary>
+    public class Gender
+    {
+        public string Name { get; }
+
+        //Конструктор
+        public Gender(string name)
+        {
+            if(string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentNullException("name пустое или null, а это н едопустимо", nameof(name));
+            }
+            Name = name;
+        }
+
+    }
+}
--- fitnessApp/fitnessApp.CMD/Program.cs
+++ fitnessApp/fitnessApp.CMD/Program.cs
@@ -4,9 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace fitnessApp.BL
+namespace fitnessApp.CMD
 {
-    public class Class1
+    internal class Program
     {
+        static void Main(string[] args)
+        {
+        }
     }
 }
